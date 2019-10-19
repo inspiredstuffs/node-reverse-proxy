03bdc79275cc7520700c549bf31745c276ddc187
Samuel Olugbemi <samuel.olugbemi@pzbp.co.uk>
2019-10-19 15:40:03 +0100

    Snagging

--- __tests__/with-restriction-spec.js
+++ __tests__/with-restriction-spec.js
@@ -18,13 +18,13 @@ describe('Reverse Proxy e2e testing with URL restrictions', function () {
   });
 
   describe('Forwarding requests sent to this server to an upstream server via a proxy server', function () {
-    it('does not forward requests without the restricted url path', function () {
+    it('does not forward requests of url that does not match the restriction string', function () {
       request('http://127.0.0.1:8888/test', function (error, response, body) {
         expect(body).not.to.contain("request successfully proxied!")
       }).end();
     });
 
-    it('forward requests to the target server only on the restricted url', function () {
+    it('forward requests of url that matches the restriction string', function () {
       request('http://127.0.0.1:8888/api/test', function (error, response, body) {
         expect(body).to.contain("request successfully proxied!")
         expect(response.statusCode).to.eql(200)
